f2d0f1c244429d7d6847d12a4029d936c8a87dee
Jeremy Parra <jeremydeanparra@gmail.com>
2019-01-06 15:18:10 -0700

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendArray = [
+    {
+      name: "Cobra Commander",
+      photoURL: "https://m.media-amazon.com/images/M/MV5BZDcyODE5MjktYjVmYy00NmExLWI0NGYtNzhmOWFhZTE1YzA3XkEyXkFqcGdeQXVyNzU1NzE3NTg@._V1_CR0,45,480,270_AL_UX477_CR0,0,477,268_AL_.jpg",
+      scores:[
+        4,
+        2,
+        5,
+        2,
+        1,
+        4,
+        5,
+        5,
+        5,
+        1
+      ],
+    },
+    {
+      name: "Megatron",
+      photoURL: "https://thedailygrindshow.files.wordpress.com/2013/03/megatron.jpg",
+      scores:[
+        4,
+        2,
+        1,
+        2,
+        3,
+        4,
+        1,
+        5,
+        3,
+        1
+      ],
+    },
+    {
+      name: "Skelator",
+      photoURL: "https://www.tvovermind.com/wp-content/uploads/2017/03/Skeletor.jpg",
+      scores:[
+        2,
+        2,
+        5,
+        3,
+        1,
+        4,
+        1,
+        1,
+        3,
+        4
+      ],
+    },
+    {
+      name: "The Shredder",
+      photoURL: "https://media.giphy.com/media/etkc18G9IrrlS/giphy.gif",
+      scores:[
+        3,
+        2,
+        2,
+        5,
+        2,
+        2,
+        1,
+        5,
+        2,
+        4
+      ],
+    },
+    {
+      name: "Gargamel",
+      photoURL: "https://i.pinimg.com/originals/93/62/bb/9362bb563cac4c767c4c3bef54661100.jpg",
+      scores:[
+        1,
+        1,
+        1,
+        2,
+        2,
+        3,
+        3,
+        4,
+        4,
+        5
+      ],
+    },
+    {
+      name: "Dr. Claw",
+      photoURL: "http://www.cinemacats.com/wp-content/uploads/television/inspectorgadgetwinterland01.jpg",
+      scores:[
+        1,
+        2,
+        3,
+        4,
+        5,
+        3,
+        4,
+        3,
+        2,
+        1
+      ],
+    },
+    {
+      name: "Mr. Burns",
+      photoURL: "https://s.yimg.com/ny/api/res/1.2/TRa9brMv0pCjuKn.GQ.tsQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/http://media.zenfs.com/en-US/homerun/entertainment_weekly_785/08cd0316d30327e46d544b2a398f4fd4",
+      scores:[
+        2,
+        4,
+        5,
+        2,
+        5,
+        2,
+        3,
+        1,
+        4,
+        5
+      ],
+    },
+    {
+      name: "Mojo Jojo",
+      photoURL: "https://vignette.wikia.nocookie.net/powerpuff/images/c/c9/Mojo_jojo_aparincia2.png/revision/latest?cb=20160612021838",
+      scores:[
+        2,
+        2,
+        2,
+        3,
+        5,
+        1,
+        3,
+        2,
+        4,
+        5
+      ],
+    },
+    {
+      name: "The Joker",
+      photoURL: "https://list.lisimg.com/image/7103140/500full.jpg",
+      scores:[
+        3,
+        2,
+        5,
+        5,
+        1,
+        3,
+        5,
+        1,
+        3,
+        1
+      ],
+    },
+    {
+      name: "Mumm-Ra",
+      photoURL: "https://iv1.lisimg.com/image/7305829/619full.jpg",
+      scores:[
+        1,
+        3,
+        5,
+        3,
+        5,
+        2,
+        2,
+        5,
+        3,
+        4
+      ],
+    },
+  ];
+  
+module.exports = friendArray;
